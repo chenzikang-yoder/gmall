03ed430a34d4b6cb2d514b20661e39b8cdcd8150
Administrator <m13570922185@aliyun.com>
2020-03-08 18:01:17 +0800

    第四天

---
+++ gmall-mange-web/src/main/java/com/atguigu/gmall/mange/controller/SpuController.java
@@ -0,0 +1,37 @@
+package com.atguigu.gmall.mange.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.PmsProductInfo;
+import com.atguigu.gmall.service.SpuService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Controller
+@CrossOrigin
+public class SpuController {
+
+    @Reference
+    SpuService spuService;
+
+    @RequestMapping("fileUpload")
+    @ResponseBody
+    public String fileUpload(@RequestParam("file") MultipartFile multipartFile){
+        String img="https://m.360buyimg.com/babel/jfs/t5137/20/1794970752/352145/d56e4e94/591417dcN4fe5ef33.jpg";
+        return img;
+    }
+    @RequestMapping("saveSpuInfo")
+    @ResponseBody
+    public String saveSpuInfo(@RequestBody PmsProductInfo pmsProductInfo){
+        return "success";
+    }
+
+    @RequestMapping("spuList")
+    @ResponseBody
+    public List<PmsProductInfo> spuList(String catalog3Id){
+        List<PmsProductInfo> pmsProductInfos = spuService.spuList(catalog3Id);
+        return pmsProductInfos;
+    }
+}
